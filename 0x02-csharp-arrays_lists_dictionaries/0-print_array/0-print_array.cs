2243d892358074b40119841d340f72d0134beef6
JoseMarulanda <josejumarumol@gmail.com>
2020-12-02 23:55:46 -0500

    first task

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Array
+{
+	public static int[] CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+		}
+
+		int[] numbers = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			numbers[i] = i;
+		}
+		Console.WriteLine("{0}", String.Join(" ", numbers));
+		return numbers;
+	}
+}
